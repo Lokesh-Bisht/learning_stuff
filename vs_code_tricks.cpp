484a01a134f05df66a55a54542af027d4c77f36a
lb18 <40322896+lb18@users.noreply.github.com>
2020-07-07 12:35:58 +0530

    Create vs_code_tricks.cpp

---
+++ vs_code_tricks.cpp
@@ -0,0 +1,18 @@
+
+void vs_code_shortcuts()
+{
+	// ctrl+shift+~		>	adds terminal window with path set to the current directory
+	// ctrl+shift+p		>	opens command palette
+}
+
+
+
+void command_palette()
+{
+	// Developer: Reload Window		>		to reload current VS code window.
+	// Developer: Open Log file..   >		to open logs 
+	// Insert Snippet				>		to insert already existing snippets into your code.
+	// Flutter: New Project			>		to create a new project
+	// Flutter: Run Flutter Doctor  >		to run doctor
+	// Flutter: Launch Emulator		>		to launch android emulator
+}
